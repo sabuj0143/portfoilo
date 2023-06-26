67ac2a2f09782d015f1767a7a0c6e9683c3eb302
sabuj0143 <sabujhowlader011@gmai.com>
2023-06-27 02:10:53 +0600

    More update Project route

--- src/component/Projects/Projects.jsx
+++ src/component/Projects/Projects.jsx
@@ -1,25 +1,67 @@
+import bannerImg from '../../../public/banner/profile-img.jpg';
+
+
 
 const Projects = () => {
     return (
-        <>
-            <div className="text-center my-4">
-                <h3 className="text-5xl font-extrabold">Projects is coming soon....</h3>
+        <div className='my-4 md:my-12'>
+            <div className="text-center">
+                <h1 className='text-4xl font-bold'>All Projects</h1>
+                <span className='relative -top-0 inline-block w-28 border-[3px] border-primary'></span>
             </div>
-            <div className="card w-96 bg-base-100 shadow-xl">
-                <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">
-                        Shoes!
-                        <div className="badge badge-secondary">NEW</div>
-                    </h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <div className="badge badge-outline">Fashion</div>
-                        <div className="badge badge-outline">Products</div>
+            <div className="w-[100%] mx-auto p-6 gap-4 md:grid grid-cols-3">
+                <div className="card w-96 h-[65vh] bg-base-200 shadow-xl rounded-xl">
+                    <figure><img className='w-full h-[40vh] p-4 rounded-xl' src={"https://i.ibb.co/6tbgqMr/academy.png"} alt="Shoes" /></figure>
+                    <div className="m-5">
+                        <div className="card-actions justify-center">
+                            <div className="badge badge-secondary">React</div>
+                            <div className="badge badge-primary">JavaScript</div>
+                            <div className="badge badge-warning">JQuery</div>
+                            <div className="badge badge-info">Express</div>
+                            <div className="badge badge-accent">Firebase</div>
+                            <div className="badge badge-error">MongoDb</div>
+                        </div>
+                        <div className='text-center'>
+                            <h3 className='mt-4 text-2xl font-bold'>Food Academy</h3>
+                            <p>A fully responsive Academy Classes provider</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="card w-96 h-[65vh] bg-base-200 shadow-xl rounded-xl">
+                    <figure><img className='w-full h-[40vh] p-4 rounded-xl' src={"https://i.ibb.co/dgwMTNs/bistro-Boss.png"} alt="Shoes" /></figure>
+                    <div className="m-5">
+                        <div className="card-actions justify-center">
+                            <div className="badge badge-secondary">React</div>
+                            <div className="badge badge-accent">React Route</div>
+                            <div className="badge badge-primary">JavaScript</div>
+                            <div className="badge badge-info">Express</div>
+                            <div className="badge badge-primary">MongoDb</div>
+                            <div className="badge badge-secondary">Firebase</div>
+                        </div>
+                        <div className='text-center'>
+                            <h3 className='mt-4 text-2xl font-bold'>Bistro Boss</h3>
+                            <p>A Home responsive E-Commerce provider</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="card w-96 h-[65vh] bg-base-200 shadow-xl rounded-xl">
+                    <figure><img className='w-full h-[40vh] p-4 rounded-xl' src={"https://i.ibb.co/X5rQ4dv/teddy.png"} alt="Shoes" /></figure>
+                    <div className="m-5">
+                        <div className="card-actions justify-center">
+                            <div className="badge badge-secondary">React</div>
+                            <div className="badge badge-accent">React Route</div>
+                            <div className="badge badge-primary">JavaScript</div>
+                            <div className="badge badge-info">Express</div>
+                            <div className="badge badge-primary">MongoDb</div>
+                        </div>
+                        <div className='text-center'>
+                            <h3 className='mt-4 text-2xl font-bold'>Teddy Bear</h3>
+                            <p>A Fully responsive E-Commerce provider</p>
+                        </div>
                     </div>
                 </div>
             </div>
-        </>
+        </div>
     );
 };
 
