73946ba22e07c0d0c781679194c7300a884412bd
sabuj0143 <sabujhowlader011@gmai.com>
2023-06-27 11:33:08 +0600

    Project route done

--- src/component/Projects/Projects.jsx
+++ src/component/Projects/Projects.jsx
@@ -1,4 +1,4 @@
-import bannerImg from '../../../public/banner/profile-img.jpg';
+import './Projects.css'
 
 
 
@@ -10,8 +10,8 @@ const Projects = () => {
                 <span className='relative -top-0 inline-block w-28 border-[3px] border-primary'></span>
             </div>
             <div className="w-[100%] mx-auto p-6 gap-4 md:grid grid-cols-3">
-                <div className="card w-96 h-[65vh] bg-base-200 shadow-xl rounded-xl">
-                    <figure><img className='w-full h-[40vh] p-4 rounded-xl' src={"https://i.ibb.co/6tbgqMr/academy.png"} alt="Shoes" /></figure>
+                <div className="card w-96 relative h-[70vh] bg-base-200 shadow-xl rounded-xl">
+                    <figure><img className='img-card w-full h-[40vh] p-4 rounded-xl' src={"https://i.ibb.co/6tbgqMr/academy.png"} alt="Shoes" /></figure>
                     <div className="m-5">
                         <div className="card-actions justify-center">
                             <div className="badge badge-secondary">React</div>
@@ -25,9 +25,14 @@ const Projects = () => {
                             <h3 className='mt-4 text-2xl font-bold'>Food Academy</h3>
                             <p>A fully responsive Academy Classes provider</p>
                         </div>
+                        <div className='flex items-center justify-around text-info cursor-pointer my-2'>
+                            <a href='https://github.com/sabuj0143/food-enthusiast-academy'>Client Code</a>
+                            <a href='https://github.com/sabuj0143/food-enthusiast-academy-server'>Server Code</a>
+                            <a href='https://enthusiast-academy.web.app/'>Live Site</a>
+                        </div>
                     </div>
                 </div>
-                <div className="card w-96 h-[65vh] bg-base-200 shadow-xl rounded-xl">
+                <div className="card w-96 h-[70vh] bg-base-200 shadow-xl rounded-xl">
                     <figure><img className='w-full h-[40vh] p-4 rounded-xl' src={"https://i.ibb.co/dgwMTNs/bistro-Boss.png"} alt="Shoes" /></figure>
                     <div className="m-5">
                         <div className="card-actions justify-center">
@@ -42,12 +47,19 @@ const Projects = () => {
                             <h3 className='mt-4 text-2xl font-bold'>Bistro Boss</h3>
                             <p>A Home responsive E-Commerce provider</p>
                         </div>
+                        <div className='flex items-center justify-around text-info my-2'>
+                            <a href='https://github.com/sabuj0143/bistro-boss-client'>Client Code</a>
+                            <a href='https://github.com/sabuj0143/bistro-bss-server'>Server Code</a>
+                            <a href='https://bistro-boss-1fbd9.web.app/'>Live Site</a>
+                        </div>
                     </div>
                 </div>
-                <div className="card w-96 h-[65vh] bg-base-200 shadow-xl rounded-xl">
+                <div className="card w-96 h-[70vh] bg-base-200 shadow-xl rounded-xl">
                     <figure><img className='w-full h-[40vh] p-4 rounded-xl' src={"https://i.ibb.co/X5rQ4dv/teddy.png"} alt="Shoes" /></figure>
                     <div className="m-5">
                         <div className="card-actions justify-center">
+                            <div className="badge badge-secondary">HTML</div>
+                            <div className="badge badge-accent">CSS</div>
                             <div className="badge badge-secondary">React</div>
                             <div className="badge badge-accent">React Route</div>
                             <div className="badge badge-primary">JavaScript</div>
@@ -57,6 +69,11 @@ const Projects = () => {
                         <div className='text-center'>
                             <h3 className='mt-4 text-2xl font-bold'>Teddy Bear</h3>
                             <p>A Fully responsive E-Commerce provider</p>
+                        </div>
+                        <div className='flex items-center justify-around text-info  my-2'>
+                            <a href='https://github.com/sabuj0143/toy-teddy-bear'>Client Code</a>
+                            <a href='https://github.com/sabuj0143/teddy-bear-server'>Server Code</a>
+                            <a href='https://kindle-teddy.web.app/'>Live Site</a>
                         </div>
                     </div>
                 </div>
